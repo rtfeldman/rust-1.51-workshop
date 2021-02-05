99716e8f6f3a255a3cf32f26d4f2a89a03ff9747
Richard Feldman <oss@rtfeldman.com>
2021-02-05 07:40:29 -0500

    Fix part1

--- part1/city.rs
+++ part1/city.rs
@@ -1,31 +1,31 @@
 pub fn main() {
-    let city_name = "rustville";
+    let city_name = "Rustville";
 
-    println!("the city of {}:\n", city_name);
+    println!("The city of {}:\n", city_name);
 
     print_population(1_324_578, 114_293, 108_097);
 }
 
 fn print_population(adults: u64, kids: u32, buildings: u32) {
-    // 👉 todo compute population by adding adults to kids
+    // 👉 TODO compute population by adding adults to kids
     //
-    // 💡 tip: use the `as` keyword to convert between numeric types!
+    // 💡 TIP: Use the `as` keyword to convert between numeric types!
     let population = 0;
 
-    // 👉 todo compute buildings_per_person by dividing population by buildings
+    // 👉 TODO compute buildings_per_person by dividing population by buildings
     //
-    // 💡 tip: to get a f64 answer here, both numerator and denominator must be f64 values
+    // 💡 TIP: To get a f64 answer here, both numerator and denominator must be f64 values
     let buildings_per_person = 0.0;
 
-    println!("    population: {}", population);
-    println!("        adults: {}", adults);
-    println!("        kids: {}", kids);
-    println!("    buildings: {}", buildings);
-    println!("    buildings per person: [👉 todo print buildings_per_person here]\n");
+    println!("    Population: {}", population);
+    println!("        Adults: {}", adults);
+    println!("        Kids: {}", kids);
+    println!("    Buildings: {}", buildings);
+    println!("    Buildings per person: [👉 TODO print buildings_per_person here]\n");
 
     if buildings_per_person >= 1.0 {
-        println!("everyone can have their own building!");
+        println!("Everyone can have their own building!");
     } else {
-        println!("buildings must be shared!");
+        println!("Buildings must be shared!");
     }
 }
