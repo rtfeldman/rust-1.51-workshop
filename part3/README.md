a7fa568f9604e59f003180decc448505d49c9a73
Richard Feldman <oss@rtfeldman.com>
2021-02-05 01:39:08 -0500

    Update part3

---
+++ part3/README.md
@@ -0,0 +1,18 @@
+# Part 1
+
+To build everything, `cd` into the `part1/` directory and run:
+
+```shell
+rustc city.rs
+```
+
+This will compile your `city.rs` file into `city` (or `city.exe` on Windows).
+
+Then you can run it with `./city` (or `city.exe` on Windows).
+
+## Exercise
+
+Open `city.rs` in your editor and resolve the TODOs there.
+
+After you complete each one, re-run `rustc city.rs` to
+recompile the `city` executable, then re-run the executable to see the output!
---
+++ part3/numbers.rs
@@ -0,0 +1,43 @@
+pub fn main() {
+    let numbers = vec![2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
+
+    println!("Sum of these numbers: {}", sum(numbers));
+    println!("Product of these numbers: 👉 TODO call product() and put answer here");
+    println!("Average of these numbers: 👉 TODO call average() and put answer here");
+
+    // 💡 TIP: You can do this in one of two ways. Try 'em both!
+    //
+    // Option 1: Pass numbers.clone() some of the time.
+    //           (Experiment to see when it's needed!)
+    //
+    // Option 2: Change some of the functions to return a tuple
+    //           of (i64, Vec<i64>), with the Vec<i64> being the
+    //           `numbers` argument that was passed in. With this
+    //           approach, you won't need to call .clone() at all!
+}
+
+fn sum(numbers: Vec<i64>) -> i64 {
+    let mut total = 0;
+
+    for num in numbers.iter() {
+        total += num;
+    }
+
+    total
+}
+
+fn product(numbers: Vec<i64>) -> i64 {
+    let mut total = 1;
+
+    for num in numbers.iter() {
+        total *= num;
+    }
+
+    total
+}
+
+fn average(numbers: Vec<i64>) -> i64 {
+    let length = numbers.len() as i64;
+
+    sum(numbers) / length
+}
