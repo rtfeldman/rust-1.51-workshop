80c97d8f2cdb3206a39dc7c7dd7b9d2cf30ceb7a
Richard Feldman <oss@rtfeldman.com>
2021-02-06 12:30:13 -0500

    Add part2

---
+++ part2/city.rs
@@ -0,0 +1,58 @@
+enum CitySize {
+    Town,       // approximate residents: 1_000
+    City,       // approximate residents: 10_000
+    Metropolis, // approximate residents: 1_000_000
+}
+
+struct City {
+    description: String,
+    residents: u64,
+}
+
+impl City {
+    fn new(city_size: CitySize) -> City {
+        let (description, residents) = match city_size {
+            CitySize::Town => {
+                let residents = 1_000;
+
+                (
+                    format!("a *town* of approximately {} residents", residents),
+                    residents,
+                )
+            }
+            // 👉 TODO Handle the other CitySize variants individually,
+            //    in a similar way to how *town* is handled here
+            _ => {
+                let residents = 1_000;
+
+                (
+                    format!(
+                        "an *unknown-size city* of approximately {} residents",
+                        residents
+                    ),
+                    residents,
+                )
+            }
+        };
+
+        // 👉 TODO Use `description` and `residents` to populate this struct
+        City {
+            description: String::new(),
+            residents: 0,
+        }
+    }
+}
+
+pub fn main() {
+    // 👉 TODO Use City::new() to create a Metropolis-sized city here
+    let rustville = City {
+        description: String::new(),
+        residents: 0,
+    };
+
+    println!("This city is {}", rustville.description);
+
+    if rustville.residents > 100_000 {
+        println!("Wow!");
+    }
+}
