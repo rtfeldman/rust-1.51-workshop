4d516ce8ab7dabcce26a5fdb4015713efbfbe088
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-04-08 19:57:35 +0530

    Hello

--- part1/src/main.rs
+++ part1/src/main.rs
@@ -7,21 +7,14 @@ fn main() {
 }
 
 fn print_population(adults: u64, kids: u32, buildings: u32) {
-    // 👉 TODO compute population by adding adults to kids
-    //
-    // 💡 TIP: Use the `as` keyword to convert between numeric types!
-    let population = 0;
-
-    // 👉 TODO compute buildings_per_person by dividing buildings by population
-    //
-    // 💡 TIP: To get a f64 answer here, both numerator and denominator must be f64 values
-    let buildings_per_person = 0.0;
+    let population = adults + kids as u64;
+    let buildings_per_person = buildings as f64 / population as f64;
 
     println!("    Population: {}", population);
     println!("        Adults: {}", adults);
     println!("        Kids: {}", kids);
     println!("    Buildings: {}", buildings);
-    println!("    Buildings per person: [👉 TODO print buildings_per_person here]\n");
+    println!("    Buildings per person: {}\n", buildings_per_person);
 
     if buildings_per_person >= 1.0 {
         println!("Everyone can have their own building!");
